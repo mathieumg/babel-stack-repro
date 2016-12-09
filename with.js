17d9163318189e0bcc40483fe0d9f1e0545f0b8e
Mathieu M-Gosselin <mathieumg@gmail.com>
2016-12-09 08:58:11 -0500

    With es2015 plugins.

--- with.js
+++ with.js
@@ -1,2 +1,10 @@
-require("babel-register")();
+require("babel-register")({
+  plugins: [
+    ["transform-es2015-classes", {loose: true}],
+    ["babel-plugin-transform-builtin-extend", {
+      globals: ["Error"],
+      approximate: true,
+    }],
+  ],
+});
 require("./without");
--- without.js
+++ without.js
@@ -11,3 +11,7 @@ console.log(test.stack);
 const test2 = new SubSubError();
 
 console.log(test2.stack);
+
+console.log("test instanceof Error", test instanceof Error);
+console.log("test instanceof SubError", test instanceof SubError);
+console.log("test2 instanceof SubError", test2 instanceof SubError);
